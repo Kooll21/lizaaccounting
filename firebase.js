56c7d63476e20e1e58dc0e79cc1c2bc4277185c2
Kooll21 <z.lebedev@seznam.cz>
2026-03-10 12:42:14 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,15 @@
 // firebase.js
-import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
+import {
+  initializeApp, getApps, getApp
+} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBNuCWtVCpC2HmkncirONfPEUpICHAFJc0",
@@ -12,6 +20,24 @@ const firebaseConfig = {
   appId: "1:407944794362:web:c646f7670a8717a3fab943"
 };
 
+// Гарантия единственной инициализации
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+// Экспорт готовых инстансов
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// 👇 Реэкспорт «боевых» функций, чтобы импортировать их из одного места
+export {
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+
+export {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
